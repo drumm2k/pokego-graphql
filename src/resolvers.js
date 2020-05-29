2f0f4d38b5a81bcd2fe0558644c5d203dcad9663
Ruslan Dzyubanov <drumm2k@gmail.com>
2020-05-29 07:29:50 +0300

    Init
    apollo-server, graphql, nodemon

---
+++ src/resolvers.js
@@ -0,0 +1,16 @@
+module.exports = {
+  Query: {
+    books: () => books,
+  },
+};
+
+const books = [
+  {
+    title: 'Harry Potter and the Chamber of Secrets',
+    author: 'J.K. Rowling',
+  },
+  {
+    title: 'Jurassic Park',
+    author: 'Michael Crichton',
+  },
+];
