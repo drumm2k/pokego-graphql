e0597d0bf6b469c12aa0093ed7fc5645bc78ead6
Ruslan Dzyubanov <drumm2k@gmail.com>
2020-05-30 08:04:28 +0300

    Fixed Events IDs, switched to apollo-express

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-const { ApolloServer, gql } = require('apollo-server');
+const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
+const server = new ApolloServer({ typeDefs, resolvers });
 
-server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
-});
+const app = express();
+server.applyMiddleware({ app });
+
+app.listen({ port: 4000 }, () =>
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+);
