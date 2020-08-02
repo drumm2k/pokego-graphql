efbb82b46d83d8ac7e7bf369b200771227299f58
Ruslan Dzyubanov <drumm2k@gmail.com>
2020-08-02 19:52:50 +0300

    Turn on secure in refresh token cookie

--- src/sendRefreshToken.js
+++ src/sendRefreshToken.js
@@ -1,6 +1,7 @@
 export const sendRefreshToken = (res, token) => {
   res.cookie('jid', token, {
     httpOnly: true,
+    secure: true,
     domain: process.env.FRONTEND_DOMAIN,
     sameSite: 'Strict',
   });
