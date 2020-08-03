4e61c8fc9d01810dc2a679589c132f47859fa9d4
Ruslan Dzyubanov <drumm2k@gmail.com>
2020-08-03 16:11:29 +0300

    Try cookie sameSite - lax

--- src/sendRefreshToken.js
+++ src/sendRefreshToken.js
@@ -1,9 +1,13 @@
 export const sendRefreshToken = (res, token) => {
+  const MAX_AGE = 7 * 24 * 60 * 60 * 1000; // 7 days
+
   res.cookie('jid', token, {
     httpOnly: true,
-    secure: true,
     // domain: process.env.FRONTEND_DOMAIN,
-    sameSite: 'none',
-    maxAge: 7 * 24 * 60 * 60 * 1000,
+    path: '/',
+    sameSite: 'lax',
+    maxAge: MAX_AGE,
+    expires: new Date(Date.now() + MAX_AGE * 1000),
+    secure: process.env.NODE_ENV === 'production',
   });
 };
